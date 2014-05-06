79947b9299a65b545ecd2b3a70321b62a8632c0c
kainlite <kainlite@gmail.com>
2014-05-06 15:49:23 -0300

    Added data in seeds files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,118 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
+## Studies
 Frame.create(
-  title: 'Lorem ipsum',
-  institution: 'Institution',
-  from: '01/01/2013',
-  to: '01/01/2014',
-  description: 'Somthing fancy...',
+  title: 'Bachelor\'s degree, informatic',
+  institution: 'E.P.E.T. Nº 4',
+  from: '01/01/1999',
+  to: '01/01/2003',
+  description: 'Professional Informatic',
   kind: 'study',
 )
 
 Frame.create(
-  title: 'Lorem ipsum 2',
-  institution: 'Institution',
+  title: 'Basic Programming and Databases',
+  institution: 'Institute Saavedra',
+  from: '01/01/2004',
+  to: '01/01/2005',
+  description: 'Basics of C, Php, ASP, javascript, MSSQL, MySQL',
+  kind: 'study',
+)
+
+Frame.create(
+  title: 'CCNA Cisco Certified Associate',
+  institution: 'ISRI',
+  from: '01/01/2005',
+  to: '01/01/2007',
+  description: 'CCNA / Networking',
+  kind: 'study',
+)
+
+Frame.create(
+  title: 'ESR',
+  institution: 'Proydesa',
+  from: '01/01/2008',
+  to: '01/01/2009',
+  description: 'Enterprise Security & Risk',
+  kind: 'study',
+)
+
+Frame.create(
+  title: 'Ruby on Rails'
+  institution: 'UTN FRM'
+  from: '01/01/2011',
+  to: '01/12/2011',
+  description: 'Ruby on rails course',
+  kind: 'study',
+)
+
+Frame.create(
+  title: 'M101P'
+  institution: 'MongoDB University'
+  from: '01/01/2013',
+  to: '01/12/2013',
+  description: 'Ruby on rails course',
+  kind: 'study',
+)
+
+Frame.create(
+  title: 'M102'
+  institution: 'MongoDB University'
+  from: '01/01/2013',
+  to: '01/12/2013',
+  description: 'Ruby on rails course',
+  kind: 'study',
+)
+
+Frame.create(
+  title: 'General Game Playing'
+  institution: 'Coursera'
+  from: '01/01/2013',
+  to: '01/12/2013',
+  description: 'Ruby on rails course',
+  kind: 'study',
+)
+
+## Work
+Frame.create(
+  title: 'Sysadmin/Programmer/DevOps - Buenos Aires',
+  institution: 'Workjoy Argentina S.A.',
+  from: '01/08/2008',
+  to: '12/12/2014',
+  description: 'Infrastructure designer/implementator, Build systems to control and improve response times on sysadmin tasks, develop and deploy management system.',
+  kind: 'work',
+)
+
+Frame.create(
+  title: 'Programmer',
+  institution: 'Cirope S.A.',
+  from: '01/01/2013',
+  to: '31/05/2014',
+  description: 'Ruby Programmer, using Rails framework',
+  kind: 'work',
+)
+
+Frame.create(
+  title: 'Computer Technician',
+  institution: 'Norfix S.A. - Buenos Aires',
+  from: '01/04/2008',
+  to: '01/08/2008',
+  description: 'Tech support, Pc repairment, Networking',
+  kind: 'work',
+)
+
+Frame.create(
+  title: 'Sysadmin',
+  institution: 'S.T.I. Informatica - San Juan',
+  from: '01/08/2005',
+  to: '01/02/2008',
+  description: 'Network administration',
+  kind: 'work',
+)
+
+Frame.create(
+  title: 'Computer Technician',
+  institution: 'CyberPoints',
   from: '01/01/2013',
   to: '01/01/2014',
-  description: 'Somthing fancy...',
+  description: 'Pc repairment, Network administration',
   kind: 'work',
 )
